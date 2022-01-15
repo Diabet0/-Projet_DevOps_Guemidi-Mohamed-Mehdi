0db24e11db6e60bac9a564da3a0b47297e278e65
Diabet0 <Makmidi@gmail.com>
2022-01-15 11:29:17 +0100

    Jenkins Fini

---
+++ terraform/conf/jobs/terraform-destroy-pipeline.groovy
@@ -0,0 +1,15 @@
+
+pipeline {
+    agent any
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '100'))
+        ansiColor('xterm')
+    }
+    stages {
+        stage('go to instance workspace folder') {
+            steps {
+                sh 'cd /var/jenkins_home/workspace/IaC/terraform && terraform destroy -auto-approve'
+            }
+        }
+    }
+}
